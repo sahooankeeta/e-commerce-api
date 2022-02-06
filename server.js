3cafecb751691fd32f57e53672d1fb19c37a167b
Ankeeta Sahoo <sahooankeeta@gmail.com>
2022-02-07 00:33:53 +0530

    initial

---
+++ server.js
@@ -0,0 +1,8 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+
+const port = process.env.PORT || 5000;
+
+server.use(router);
+server.listen(port, () => console.log("running"));
