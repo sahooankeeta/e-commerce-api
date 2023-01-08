b0fafe76fdd2c7e2331c1ce295beba7ff53470cd
Ankeeta Sahoo <sahooankeeta@gmail.com>
2023-01-08 21:55:10 +0530

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
 const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
-server.use(middlewares);
-server.use(router);
-server.listen(port);
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use(router)
+server.listen(process.env.PORT || 8000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
